6eec1ac48e1e1e7a0ef6c64df628c121badacc7e
ChikereNjoku Chibuike <cchikerenjoku@weber.edu>
2017-02-14 16:39:07 +0000

    initial commit

---
+++ jpsi_Hw5_Task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  jpsi_Hw5_Task1.c
+ *       	Usage:  ./jpsi_Hw5_Task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:37:31 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Chikere-Njoku Chibuike (), cchikerenjoku@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O*/
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
