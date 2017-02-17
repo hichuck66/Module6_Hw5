b86c1c62bf4ea75dc27434c383170819f2535fa3
ChikereNjoku Chibuike <cchikerenjoku@weber.edu>
2017-02-17 16:57:48 +0000

    jpsi_Hw5_Task1

--- jpsi_Hw5_Task1.c
+++ jpsi_Hw5_Task1.c
@@ -3,29 +3,60 @@
  *
  *       Filename:  jpsi_Hw5_Task1.c
  *       	Usage:  ./jpsi_Hw5_Task1.c
- *    Description:  
+ *    Description:  Calculating the second inertia, given radius of inside and outside
  *
  *        Version:  1.0
  *        Created:  02/14/2017 04:37:31 PM
- *       Compiler:  gcc -Wall -Werror
+ *       Compiler:  gcc -Wall -Werror -lm (presence of math.h)
  *         Author:  Chikere-Njoku Chibuike (), cchikerenjoku@mail.weber.edu
  * =====================================================================================
- */
-#include <stdio.h>		/* For Standard I/O*/
+ 
+ //For standard I/O */
+
+#include <stdio.h>
+#include <math.h>
+#define pi 3.14159265359
 
 /* Function Prototypes */
+//declare function//
+
+void AnnulusMoment(double, double);
 
 /* Main Program */
 int main(void)
 {
 
-//can you see this
-//
-//no wtf moment now
+	double Rout, Rin;
+
+printf("\nEnter the value of outer radius(Rout): ");
+scanf("%lf", &Rout);
+
+printf("\nNow enter the value for the inner radius(Rin): ");
+scanf("%lf", &Rin);
+
+AnnulusMoment(Rout, Rin); //calling the function//
+
 	return 0;
 }
 
+/* Function Definitions */
 
-/* Function Defenitions */
+
+void AnnulusMoment(double Rout, double Rin)
+{
+
+float I;
+
+I = (pi/4.0 * pow(Rout,4) - pow(Rin,4));
+
+printf("\nThe second moment of inertia is %lf\n\n", I);
+
+printf("DONE");
+
+return;
+}
+
+
+
 
 
