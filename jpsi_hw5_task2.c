7d8ec296af7baf5c119608126ea48c904f6acac7
Raul Rivera <raulriverapuig@mail.weber.edu>
2017-02-17 01:05:40 +0000

    Version 1.0 of task 2

--- jpsi_hw5_task2.c
+++ jpsi_hw5_task2.c
@@ -12,18 +12,38 @@
  * =====================================================================================
  */
 #include <stdio.h>		/* For Standard I/O */
-
 /* Function Prototypes */
+
+float MakeMilesKmTable(float m);
 
 /* Main Program */ // Task 2 // 
 int main(void)
 {
-// w
-// xD
+	float miles, end, answer;
+	printf("Enter the starting miles: \n");
+	scanf("%f",&miles);
+	printf("Enter the ending miles: \n");
+	scanf("%f",&end);
+	int i=0,j=end-miles;       // This is how many times it will print
+	printf(" Miles    Kilometers\n");
+	printf(" -----    ----------\n");
+		while (i<=j)
+	{
+		answer = MakeMilesKmTable(miles);
+		printf("%5.1f   %9.2f\n",miles,answer);
+		miles++;               // Update the miles it will convert to Km
+		i++;
+	}
+		printf("\n");          // Style points
 	return 0;
 }
 
 
 /* Function Defenitions */
-
+float MakeMilesKmTable(float miles) 
+{
+	float km;
+	km = miles * 1.61;
+	return km;
+}
 
